c7eeefb70e7bcae7a622472d7cdd931185aba5c5
Yana-crypto702 <yanaorlova_1@icloud.com>
2024-08-10 15:52:11 +0400

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -2,7 +2,7 @@ immutable_tuple= (1, 2, 'a', 'b')
 print(immutable_tuple)
 #immutable_tuple[0]=200
 #print(immutable_tuple)  #мы не сможем вывести на экран такое действие, т.к. кортеж-является неизменным объектом
-Mutablelist=([1, 2, 'a', 'b',], 'Modified')
-print(Mutablelist)
-Mutablelist[0][0]=46
-print(Mutablelist)
+mutable_list=[1, 2, 'a', 'b']
+print(mutable_list)
+mutable_list[0]=46
+print(mutable_list)
